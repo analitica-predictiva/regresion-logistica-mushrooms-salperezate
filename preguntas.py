2a45af3671d49e345ea436e42eca463993b96d11
salperezate <salperezate@unal.edu.co>
2023-11-26 13:58:16 -0500

    Correccion p1

--- preguntas.py
+++ preguntas.py
@@ -64,13 +64,11 @@ def pregunta_01():
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    #df = ____
     df = pd.read_csv("mushrooms.csv")
 
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
-    #____.____(____)
-    df.drop("veil-type", axis=1, inplace=True)
+    df.pop("veil-type")
 
     # Asigne la columna `type` a la variable `y`.
     #____ = ____
@@ -82,7 +80,7 @@ def pregunta_01():
 
     # Remueva la columna `type` del DataFrame `X`.
     #____.____(____)
-    X.drop("type", axis=1, inplace=True)
+    X.pop("type")
 
     # Retorne `X` y `y`
     return X, y
